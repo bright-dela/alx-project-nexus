4034ab366732f7a753c81c06a34d9faef30867c4
bright-dela <brightdelasettor@gmail.com>
2025-11-19 15:03:57 -0600

    Feat: user authentication - update third-party apps in settings

--- ecommerce_backend/config/settings.py
+++ ecommerce_backend/config/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",   
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+
 ]
 
 MIDDLEWARE = [
