bf2310364b69153b0c344f500646e194bd051879
bright-dela <brightdelasettor@gmail.com>
2025-11-25 22:14:08 +0000

    feat: dockernize the django app, with redis and celery services

--- e-commerce-backend/nexus/settings.py
+++ e-commerce-backend/nexus/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "nexus.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
