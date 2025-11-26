8477a2cc6a067f396435949ea5011c5c293d517d
bright-dela <brightdelasettor@gmail.com>
2025-11-26 18:29:37 +0000

    fix: debug and fix error that prevents celery container from running

--- e-commerce-backend/nexus/celery.py
+++ e-commerce-backend/nexus/celery.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nexus.settings")
 
-app = Celery("config")
+app = Celery("nexus")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
