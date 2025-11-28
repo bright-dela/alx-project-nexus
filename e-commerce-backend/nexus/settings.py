7b203d706086542c36c71128a9363d4d9dc117f0
bright-dela <brightdelasettor@gmail.com>
2025-11-28 18:36:24 +0000

    update Google oauth credentials

--- e-commerce-backend/nexus/settings.py
+++ e-commerce-backend/nexus/settings.py
@@ -145,9 +145,9 @@ MEDIA_ROOT = BASE_DIR / "media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # caching settings
-CATEGORY_TREE_CACHE_TIMEOUT = config("CATEGORY_TREE_CACHE_TIMEOUT", cast=int)
-PRODUCT_LIST_CACHE_TIMEOUT = config("PRODUCT_LIST_CACHE_TIMEOUT", cast=int)
-PRODUCT_DETAIL_CACHE_TIMEOUT = config("PRODUCT_DETAIL_CACHE_TIMEOUT", cast=int)
+CATEGORY_TREE_CACHE_TIMEOUT = config("CATEGORY_TREE_CACHE_TIMEOUT", default=43200, cast=int)
+PRODUCT_LIST_CACHE_TIMEOUT = config("PRODUCT_LIST_CACHE_TIMEOUT", default=300, cast=int)
+PRODUCT_DETAIL_CACHE_TIMEOUT = config("PRODUCT_DETAIL_CACHE_TIMEOUT", default= 600, cast=int)
 
 CACHES = {
     "default": {
@@ -301,8 +301,8 @@ SIMPLE_JWT = {
 # Email Configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_USE_TLS = True
+EMAIL_PORT = config("EMAIL_PORT", default=465, cast=int)
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="")
